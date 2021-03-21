3a99255cceb2b0c33b21dc71f69a6f8894faddd5
sya-ri <sya79lua@gmail.com>
2021-03-21 20:03:26 +0900

    コマンドによる変更をファイルに反映するようにした

--- src/main/kotlin/com/github/syari/plugin/nofall/CommandCreator.kt
+++ src/main/kotlin/com/github/syari/plugin/nofall/CommandCreator.kt
@@ -15,10 +15,12 @@ object CommandCreator {
                     when (args.lowerOrNull(0)) {
                         "enable" -> {
                             ConfigLoader.isEnable = true
+                            ConfigLoader.save()
                             sender.send("&f落下ダメージを &c無効化 &fしました")
                         }
                         "disable" -> {
                             ConfigLoader.isEnable = false
+                            ConfigLoader.save()
                             sender.send("&f落下ダメージを &a有効化 &fしました")
                         }
                         "reload" -> {
--- src/main/kotlin/com/github/syari/plugin/nofall/ConfigLoader.kt
+++ src/main/kotlin/com/github/syari/plugin/nofall/ConfigLoader.kt
@@ -1,6 +1,7 @@
 package com.github.syari.plugin.nofall
 
 import com.github.syari.plugin.nofall.Main.Companion.plugin
+import com.github.syari.spigot.api.config.CustomConfig
 import com.github.syari.spigot.api.config.config
 import com.github.syari.spigot.api.config.type.ConfigDataType
 import org.bukkit.command.CommandSender
@@ -9,8 +10,18 @@ object ConfigLoader {
     var isEnable = false
 
     fun load(sender: CommandSender) {
-        plugin.config(sender, "config.yml") {
+        config(sender) {
             isEnable = get("enable", ConfigDataType.Boolean, default = false)
         }
+    }
+
+    fun save() {
+        config(null) {
+            set("enable", ConfigDataType.Boolean, isEnable, save = true)
+        }
+    }
+
+    private fun config(sender: CommandSender?, action: CustomConfig.() -> Unit) {
+        plugin.config(sender, "config.yml", action = action)
     }
 }
